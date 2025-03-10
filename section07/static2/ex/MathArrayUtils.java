00c80b63efbb30413e458b71699f2e298b1946a3
kihong kim <clevermind2@naver.com>
2025-03-10 21:19:26 +0900

    25.03.10.(월)

--- section07/static2/ex/MathArrayUtils.java
+++ section07/static2/ex/MathArrayUtils.java
@@ -1,4 +1,42 @@
-package static2.ex;
+package com.kihongkim.section07.static2.ex;
 
 public class MathArrayUtils {
+
+    private MathArrayUtils() {}
+
+    public static int sum(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static double average(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return (double) sum / values.length;
+    }
+
+    public static int min(int[] values) {
+        int minValue = values[0];
+        for (int value : values) {
+            if (minValue > value) {
+                minValue = value;
+            }
+        }
+        return minValue;
+    }
+
+    public static int max(int[] values) {
+        int maxValue = values[0];
+        for (int value : values) {
+            if (maxValue < value) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
 }
--- section07/static2/ex/MathArrayUtilsMain.java
+++ section07/static2/ex/MathArrayUtilsMain.java
@@ -1,9 +1,9 @@
-package static2.ex;
+package com.kihongkim.section07.static2.ex;
 
 public class MathArrayUtilsMain {
 
     public static void main(String[] args) {
-        int[] values = {1, 2, 3, 4, 5};
+        int[] values = {1,2,3,4,5};
         System.out.println("sum=" + MathArrayUtils.sum(values));
         System.out.println("average=" + MathArrayUtils.average(values));
         System.out.println("min=" + MathArrayUtils.min(values));
