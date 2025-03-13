9f243d66207d56f069af7f8a40dfd479af67824a
kihongkim <clevermind2@naver.com>
2025-03-13 12:29:13 +0900

    25.03.13.(목)

--- section12/poly/ex/pay1/PayStore.java
+++ section12/poly/ex/pay1/PayStore.java
@@ -4,7 +4,11 @@ public abstract class PayStore {
 
     public static Pay findPay(String option) {
         if (option.equals("kakao")) {
-            return new KakaoPay();dz
+            return new KakaoPay();
+        } else if (option.equals("naver")) {
+            return new NaverPay();
+        } else {
+            return new DefaultPay();
         }
     }
 }
