7636758599d2e0d931faa8a32179404c97830e00
kihongkim <clevermind2@naver.com>
2025-03-14 17:49:40 +0900

    25.03.14.(금)

---
+++ Mid1/section01/lang/immutable/change/ImmutableObj.java
@@ -0,0 +1,6 @@
+package com.kihongkim.Mid1.section01.lang.immutable.change;
+
+public class ImmutableObj {
+
+    private final int value;
+}
---
+++ Mid1/section01/lang/immutable/change/MutableMain.java
@@ -0,0 +1,10 @@
+package com.kihongkim.Mid1.section01.lang.immutable.change;
+
+public class MutableMain {
+
+    public static void main(String[] args) {
+        MutableObj obj = new MutableObj(10);
+        obj.add(20);
+        System.out.println("obj = " + obj.getValue());
+    }
+}
